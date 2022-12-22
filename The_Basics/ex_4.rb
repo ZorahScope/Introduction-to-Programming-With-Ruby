0f5f7d5190ac0a2b0b9316e8c66c09625ed77c57
Eli <mojojoflow@pm.me>
2022-12-21 18:49:46 -0600

    complete exercise 4

---
+++ The_Basics/ex_4.rb
@@ -0,0 +1,7 @@
+=begin
+  Use the dates from the previous example and store them in an array. Then make your program output the same thing as exercise 3.
+=end
+
+dates = [1999, 1999, 2009, 2002, 2000]
+
+dates.each {|date| puts date}
