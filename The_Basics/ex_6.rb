644316a6152861d551f6861217d26e53db86f8ca
Eli <mojojoflow@pm.me>
2022-12-21 19:11:51 -0600

    Complete exercise 6

---
+++ The_Basics/ex_6.rb
@@ -0,0 +1,6 @@
+# Write a program that calculates the squares of 3 float numbers of your choosing and outputs the result to the screen
+
+puts "Sqaure of 5.25 = " + (5.25**2).to_s
+puts "Sqaure of 9.45 = " + (9.45**2).to_s
+puts "Sqaure of 15.25 = " + (15.25**2).to_s
+
