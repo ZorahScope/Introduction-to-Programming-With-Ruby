3dc4d6690e74dcadb4ea67bb2c048bbedf2402d7
Eli <mojojoflow@pm.me>
2022-12-21 22:12:34 -0600

    Complete exercise 4

--- 02_Variables/01_name.rb
+++ 02_Variables/01_name.rb
@@ -1,8 +1,14 @@
-print "Please enter your name: "
-name = gets.chomp
-
-puts "Hello #{name.capitalize}"
+# Exercise 1
+# print "Please enter your name: "   
+# name = gets.chomp
 
 # Exercise 3
+# 10.times {puts name}
 
-10.times {puts name}
+#Exercise 4
+
+print "Please enter your first name: "
+first_name = gets.chomp
+print "Please enter your last name: "
+last_name = gets.chomp
+puts "Hello #{first_name.capitalize} #{last_name.capitalize}"
