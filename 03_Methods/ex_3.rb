3b56f8ee0e7ee3e427c157bb5ca7897ca6d39782
Eli Allen <mojojoflow@pm.me>
2022-12-25 17:20:07 -0600

    Complete exercise 3 - Chapter 3

---
+++ 03_Methods/ex_3.rb
@@ -0,0 +1,7 @@
+# Write a program that includes a method called multiply that takes two arguments and returns the product of the two numbers.
+
+def multiply(arg1, arg2)
+  arg1 * arg2
+end
+
+puts multiply 2, 4
