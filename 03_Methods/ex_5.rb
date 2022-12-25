e4fd395832a773ba84c7c75ad8e257404b23ed92
Eli Allen <mojojoflow@pm.me>
2022-12-25 17:37:38 -0600

    Complete exercise 5 - Chapter 3

---
+++ 03_Methods/ex_5.rb
@@ -0,0 +1,10 @@
+# 1) Edit the method definition in exercise #4 so that it does print words on the screen. 
+# 2) What does it return now?
+
+def scream(words)
+  p words = words + "!!!!"
+end
+
+scream("Yippeee") # => "Yippeee!!!!"
+
+
