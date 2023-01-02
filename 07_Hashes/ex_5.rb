e60a48896845cd23232877cd69abbff0720c647d
Eli <mojojoflow@pm.me>
2023-01-01 21:03:11 -0600

    Complete exercise 5 - Chapter 7

---
+++ 07_Hashes/ex_5.rb
@@ -0,0 +1,6 @@
+# What method could you use to find out if a Hash contains a specific value in it? 
+# Write a program that verifies that the value is within the hash.
+
+hsh = {one: 1, two: 2, three: 3}
+
+hsh.has_value?(3) # => true
