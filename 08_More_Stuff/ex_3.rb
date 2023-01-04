21e112b853262d4e9375d2c9fbb7b7e89fc33dec
Eli <mojojoflow@pm.me>
2023-01-03 19:05:38 -0600

    Complete exercise 3 - Chapter 8

---
+++ 08_More_Stuff/ex_3.rb
@@ -0,0 +1,3 @@
+# What is exception handling and what problem does it solve?
+
+# Allows a program to gracefully handle errors however they're decided instead of exiting the program when an error is raised
